8566d07fb2bbdc1726afac23ef42320e877c39e5
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:20:48 +0300

    HW1-Task 1

---
+++ HW1/Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A");
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            Console.WriteLine("Enter number B");
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            int result1 = A / B;
+            Console.WriteLine($"Division result = {result1}");
+            int result2 = A % B;
+            Console.WriteLine($"Remains = {result2}");
+        }
+    }
+}
