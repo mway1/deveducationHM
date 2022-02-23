67a5b49e908800aabed7c057c4e452b7a38156a0
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:38:29 +0300

    HW2-Task 1

---
+++ HW2/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A");
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            Console.WriteLine("Enter number B");
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            if (A > B)
+            {
+                Console.WriteLine($"The result of addition = {A+B}");
+            }
+            else if (A == B)
+            {
+                Console.WriteLine($"The result of multiplication = {A*B}");
+            }
+            else
+            {
+                Console.WriteLine($"The result of subtraction = {A-B}");
+            }
+            
+        }
+    }
+}
