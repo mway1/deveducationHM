0c462256ad2c827dbb2f5dd39fad669460ce7674
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:22:14 +0300

    HW1-Task 3

---
+++ HW1/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A");
+            string A = Console.ReadLine();
+            Console.WriteLine("Enter number B");
+            string B = Console.ReadLine();
+            string copyA = A;
+
+            A = B;
+            B = copyA;
+
+            Console.WriteLine($"New A: {A}");
+            Console.WriteLine($"New B: {B}");
+        }
+    }
+}
