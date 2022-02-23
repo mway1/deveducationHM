afeee86043628f50cd9969bf3291fb27e354bba8
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:22:35 +0300

    HW1-Task 4

---
+++ HW1/Task 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A");
+            string numA = Console.ReadLine();
+            int A = Convert.ToInt32(numA);
+            Console.WriteLine("Enter number B");
+            string numB = Console.ReadLine();
+            int B = Convert.ToInt32(numB);
+            Console.WriteLine("Enter number С");
+            string numС = Console.ReadLine();
+            double C = Convert.ToDouble(numС);
+
+            double x = ((C - B) / A);
+
+            Console.WriteLine($"Result: {x}");
+        }
+    }
+}
