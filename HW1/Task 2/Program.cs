5c07a50476d0288d12d95799fc4abb596e9b9590
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:21:55 +0300

    HW1-Task 2

---
+++ HW1/Task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A");
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            Console.WriteLine("Enter number B");
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            int result = (((5 * A) + (B * B)) / (B - A));
+            Console.WriteLine($"Result = {result}");
+        }
+    }
+}
