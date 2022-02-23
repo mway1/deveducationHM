ef58e868e2463796029b3fbd5382df070e9ea38a
Max Kuzmenko <mway2914@gmail.com>
2022-02-23 16:53:12 +0300

    HW2-Task 2

---
+++ HW2/Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the X coordinate");
+            string numberX = Console.ReadLine();
+            int X = Convert.ToInt32(numberX);
+            Console.WriteLine("Enter the Y coordinate");
+            string numberY = Console.ReadLine();
+            int Y = Convert.ToInt32(numberY);
+
+            Console.WriteLine($"Your point has coordinates: ({X};{Y})");
+
+            if (X > 0 && Y > 0)
+            {
+                Console.WriteLine($"Your point is in the FIRST quarter");
+            }
+            else if (X < 0 && Y > 0)
+            {
+                Console.WriteLine($"Your point is in the SECOND quarter");
+            }
+            else if (X < 0 && Y < 0)
+            {
+                Console.WriteLine($"Your point is in the THIRD quarter");
+            }
+            else
+            {
+                Console.WriteLine($"Your point is in the FOURTH quarter");
+            }
+
+        }
+    }
+}
