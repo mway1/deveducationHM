4fde94b1a9bb80d177d47e63af69f29d1fa423ee
Max Kuzmenko <mway2914@gmail.com>
2022-02-26 02:39:37 +0300

    HW3-Task 3

---
+++ HW3/Task 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            for (int i = 1; i < A; i++)
+            { 
+                if (i * i < A)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Count of numbers: {count}");
+        }
+    }
+}
