9cf92bfe3ac30d63b84e1e74b3c249d77e43b5ba
Max Kuzmenko <mway2914@gmail.com>
2022-02-26 02:51:26 +0300

    HW3-Task 4

---
+++ HW3/Task 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            int maxDivisor = 0;
+            for (int i = 1; i < A; i++)
+            {
+                if (A%i==0 && i!=A)
+                {
+                    if (i > maxDivisor)
+                    {
+                        maxDivisor = i;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine($"Maximum divisor: {maxDivisor}");
+        }
+    }
+}
