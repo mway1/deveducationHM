299667bfaa883b893f3357bc4a72a9fd4e0dc757
Max Kuzmenko <mway2914@gmail.com>
2022-02-26 02:12:15 +0300

    HW3-Task 1

---
+++ HW3/Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number A:");
+            string numberA = Console.ReadLine();
+            int A = Convert.ToInt32(numberA);
+            Console.WriteLine("Enter number B:");
+            string numberB = Console.ReadLine();
+            int B = Convert.ToInt32(numberB);
+
+            int number = 1;
+            for (int i = 0; i < B; i++)
+            {
+                number *= A;
+            }
+
+            Console.WriteLine($"The result of exponentiation: {number}");
+        }
+    }
+}
