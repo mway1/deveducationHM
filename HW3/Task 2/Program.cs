ed47faa21a69290dbce8a3f73f7b8590d37ed0d5
Max Kuzmenko <mway2914@gmail.com>
2022-02-26 02:24:43 +0300

    HW3-Task 2

---
+++ HW3/Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = A; i < 1000; i+=A)
+            {
+                if (i % A == 0)
+                {
+                    Console.WriteLine($"A number divisible by A: {i}");
+                }
+            }
+        }
+    }
+}
