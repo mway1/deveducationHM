82affa587d2964b13c98f0116a156dcdbffec49d
Max Kuzmenko <mway2914@gmail.com>
2022-02-27 15:57:37 +0300

    HW3-Task 7

---
+++ HW3/Task 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number B: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            int nod;
+            while(A!=0 && B != 0)
+            {
+                if (A > B)
+                {
+                    A = A % B;
+                }
+                else
+                {
+                    B = B % A;
+                }
+            }
+            nod = A + B;
+            Console.WriteLine($"Smallest common divisor: {nod}");
+        }
+    }
+}
