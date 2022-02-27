9b4806428d3b9f53b747daf3b2667c7b0cb338ad
Max Kuzmenko <mway2914@gmail.com>
2022-02-27 16:17:11 +0300

    HW3-Task 9

---
+++ HW3/Task 9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int tmp;
+            int count = 0;
+            while (A != 0)
+            {
+                tmp = A % 10;
+                A /= 10;
+                if (tmp % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Smallest common divisor: {count}");
+        }
+    }
+}
