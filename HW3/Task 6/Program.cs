1520fda287c0ebd227e134423260aca9219eca4c
Max Kuzmenko <mway2914@gmail.com>
2022-02-27 15:47:06 +0300

    HW3-Task 6

---
+++ HW3/Task 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int num1 = 1;
+            int num2 = 1;
+            int i = 0;
+            int sum = 0;
+                while (i < N-2)
+                {
+                    sum = num1 + num2;
+                    num1 = num2;
+                    num2 = sum;
+                    i++;
+                }
+                Console.WriteLine($"{N} element of the Fibonacci series: {num2}");
+        }
+    }
+}
