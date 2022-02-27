614ae035a96e74a4506ae3e468db6cb39499a3a6
Max Kuzmenko <mway2914@gmail.com>
2022-02-27 16:29:12 +0300

    HW3-Task 10

---
+++ HW3/Task 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int tmp;
+            int NewNum = 0;
+            while (A != 0)
+            {
+                tmp = A % 10;
+                NewNum *= 10;
+                NewNum += tmp;
+                A /= 10;
+            }
+            Console.WriteLine($"Reverse number notation: {NewNum}");
+        }
+    }
+}
