e81eb6a2a713c8e6f0d34648fc2702b8c0dc3b08
Max Kuzmenko <mway2914@gmail.com>
2022-02-27 21:57:43 +0300

    HW3-Task 11

---
+++ HW3/Task 11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int tmp;
+            int decade;
+            for (int i = 1; i < N; i++)
+            {
+                int sumeven = 0;//Сумма четных
+                int sumodd = 0;//Сумма нечетных
+                tmp = i;
+                while (tmp != 0)
+                {
+                    decade =tmp%10;
+                    if (tmp % 2 == 0)
+                    {
+                        sumeven += decade;
+                    }
+                    else
+                    {
+                        sumodd += decade;
+                    }
+                    tmp /= 10;
+                }
+                if (sumeven > sumodd)
+                {
+                    Console.WriteLine($"Result: {i}");
+                }
+            }
+        }
+    }
+}
