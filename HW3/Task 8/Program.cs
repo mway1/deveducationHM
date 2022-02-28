06c044f62730a33559094fb59e3a98dbf912c908
Max Kuzmenko <mway2914@gmail.com>
2022-02-28 13:39:44 +0300

    HW3-Task 8

---
+++ HW3/Task 8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = N;
+            int tmp = 0;
+
+            while (Math.Pow(tmp, 3) != N)
+            {
+                tmp = (a + b) / 2;
+                if(Math.Pow(tmp, 3) < N)
+                {
+                    a = tmp;
+                }
+                else
+                {
+                    b = tmp;
+                }
+            }
+            Console.WriteLine($"Result: {tmp}");
+
+
+        }
+    }
+}
