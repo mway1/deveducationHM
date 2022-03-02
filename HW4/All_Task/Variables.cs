b3a74dfca6017e388a2f64f9ace73fe6d227d82a
Maxim Kuzmenko <mway2914@gmail.com>
2022-03-02 23:12:45 +0300

    creating comments on methods in class Variables

--- HW4/All_Task/Variables.cs
+++ HW4/All_Task/Variables.cs
@@ -6,17 +6,17 @@ namespace All_Task
 {
     static class Variables
     {
-        public static int GetSolvingTheFormula(int a, int b)
+        public static int GetSolvingTheFormula(int a, int b)//HW1-Task1
         {
             int result = (((5 * a) + (b * b)) / (b - a));
             return result;
         }
-        public static double GetTheRootOfTheEquation(double a, double b, double c)
+        public static double GetTheRootOfTheEquation(double a, double b, double c)//HW1-Task2
         {
             double x = ((c - b) / a);
             return x;
         }
-        public static string GetEquationOfStraightLine(int x1, int y1, int x2, int y2)
+        public static string GetEquationOfStraightLine(int x1, int y1, int x2, int y2)//HW1-Task5
         {
             int A = (y2 - y1) / (x2 - x1);
             int B = y2 - (A * x2);
