65db4ae66829a8d3d61202a3610d9df4acf86064
Maxim Kuzmenko <mway2914@gmail.com>
2022-03-08 23:16:35 +0300

    HW5- Create methods CreateAnArrayWithRandom, OutputAnArrayToTheConsole in class Array

--- HW4/All_Task/Array.cs
+++ HW4/All_Task/Array.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace All_Task
 {
-    class Array
+    static class Array
     {
+        public static int[] CreateAnArrayWithRandom(int count)
+        {
+            int[] array = new int[count];
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = rnd.Next(-100, 100);
+            }
+            return array;
+        }
+
+        public static void OutputAnArrayToTheConsole(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+
     }
 }
