1e36d5d55b87cbd662aaf1d240808edfaf981333
Maxim Kuzmenko <mway2914@gmail.com>
2022-03-16 21:25:54 +0300

    HW7-Create tests to HW4.Task3-4

--- HW4/All_Task.Test/ArrayTasksTests.cs
+++ HW4/All_Task.Test/ArrayTasksTests.cs
@@ -6,7 +6,7 @@ namespace All_Task.Test
     class ArrayTasksTests
     {
         [TestCase(new int[] {2,3,4,5,6,7,8},2)]
-        [TestCase(new int[] {-5,-4,-3,-2,-1,0},-5)]
+        [TestCase(new int[] {-5,-4,7,-2,-1,0},-5)]
         public void GetMinElementArrayTest(int[] a, int expected)
         {
             int actual = ArrayTasks.GetMinElementArray(a);
@@ -21,7 +21,7 @@ namespace All_Task.Test
         }
 
         [TestCase(new int[] { 2, 3, 4, 5, 6, 7, 8 }, 8)]
-        [TestCase(new int[] { -5, -4, -3, -2, -1, 0 }, 0)]
+        [TestCase(new int[] { -5, -4, 7, -2, -1, 0 }, 7)]
         public void GetMaxElementArrayTest(int[] a, int expected)
         {
             int actual = ArrayTasks.GetMaxElementArray(a);
@@ -33,6 +33,36 @@ namespace All_Task.Test
         public void GetMaxElementArrayTest_WhenALowerOne_ShouldThrowException(int[] a)
         {
             Assert.Throws<Exception>(() => ArrayTasks.GetMaxElementArray(a));
+        }
+
+        [TestCase(new int[] { 2, 3, 4, 5, 6, 7, 8 }, 0)]
+        [TestCase(new int[] { -5, -4, 7, -2, -1, 0 }, 0)]
+        public void GetMinElementIndexTest(int[] a, int expected)
+        {
+            int actual = ArrayTasks.GetMinIndexArray(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { })]
+        public void GetMinElementIndexTest_WhenALowerOne_ShouldThrowException(int[] a)
+        {
+            Assert.Throws<Exception>(() => ArrayTasks.GetMinIndexArray(a));
+        }
+
+        [TestCase(new int[] { 2, 3, 4, 5, 6, 7, 8 }, 6)]
+        [TestCase(new int[] { -5, -4, 7, -2, -1, 0 }, 2)]
+        public void GetMaxElementIndexTest(int[] a, int expected)
+        {
+            int actual = ArrayTasks.GetMaxIndexArray(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { })]
+        public void GetMaxElementIndexTest_WhenALowerOne_ShouldThrowException(int[] a)
+        {
+            Assert.Throws<Exception>(() => ArrayTasks.GetMaxIndexArray(a));
         }
     }
 }
